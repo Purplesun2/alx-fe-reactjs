d12b815ec328887582dc5e9a6ad74102dd3fd6db
Ekpen <ekpenitohan@gmail.com>
2024-08-18 21:56:57 +0000

    New updates

---
+++ alx-react-app-props/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Counter from './components/Counter'; // Step 1: Import the Counter component
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to My App</h1>
+      {/* Step 2: Add the Counter component below */}
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
---
+++ alx-react-app-props/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer style={{ backgroundColor: 'black', color: 'white', textAlign: 'center', padding: '10px' }}>
+      <p>&copy; 2024 My Company</p>
+    </footer>
+  );
+}
+
+export default Footer;
