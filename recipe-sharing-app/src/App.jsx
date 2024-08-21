9bb5e152e1e722acc24b1fbac277663e524ce2c5
Ekpen <ekpenitohan@gmail.com>
2024-08-21 23:19:56 +0000

    Add new updates

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import RecipeList from './components/RecipeList.jsx';
+import AddRecipeForm from './components/AddRecipeForm.jsx';
+
+
+function App() {
+  return (
+    <div>
+      <h1>Recipe Sharing App</h1>
+      <AddRecipeForm />
+      <RecipeList />
+    </div>
+  );
+}
+
+export default App;
