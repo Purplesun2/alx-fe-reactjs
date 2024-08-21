23c4abb39042549355c326edf3e8b2c0f61d796a
Ekpen <ekpenitohan@gmail.com>
2024-08-21 19:01:16 +0000

    Add new updates

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,11 +2,19 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav style={{ padding: '10px', background: '#f0f0f0' }}>
-      <Link to="/" style={{ margin: '10px' }}>Home</Link>
-      <Link to="/about" style={{ margin: '10px' }}>About</Link>
-      <Link to="/services" style={{ margin: '10px' }}>Services</Link>
-      <Link to="/contact" style={{ margin: '10px' }}>Contact</Link>
+    <nav
+      style={{
+        backgroundColor: "#333",
+        display: "flex",
+        justifyContent: "space-around",
+        padding: "10px",
+        color: "white",
+      }}
+    >
+      <Link to="/" style={{ color: "white", textDecoration: "none" }}>Home</Link>
+      <Link to="/about" style={{ color: "white", textDecoration: "none" }}>About</Link>
+      <Link to="/services" style={{ color: "white", textDecoration: "none" }}>Services</Link>
+      <Link to="/contact" style={{ color: "white", textDecoration: "none" }}>Contact</Link>
     </nav>
   );
 }
