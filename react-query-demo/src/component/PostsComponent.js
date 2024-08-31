02b7af52995321521df45bef29929df9c60ad49d
Ekpen <ekpenitohan@gmail.com>
2024-08-31 20:06:21 +0000

    Initial commit with formcd

---
+++ react-query-demo/src/component/PostsComponent.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { useQuery } from 'react-query';
+import axios from 'axios';
+
+const fetchPosts = async () => {
+  const { data } = await axios.get('https://jsonplaceholder.typicode.com/posts');
+  return data;
+};
+
+function PostsComponent() {
+  const { data, error, isLoading, refetch } = useQuery('posts', fetchPosts);
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error occurred: {error.message}</div>;
+
+  return (
+    <div>
+      <button onClick={() => refetch()}>Refetch Posts</button>
+      <ul>
+        {data.map(post => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default PostsComponent;
