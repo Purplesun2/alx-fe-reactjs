7980d084ede7560907636a99a5103f993c08d0d2
Ekpen <ekpenitohan@gmail.com>
2024-09-01 19:34:09 +0000

    Add Jest and Testing Library for testing

--- react-todo/src/components/TodoList.jsx
+++ react-todo/src/components/TodoList.jsx
@@ -1,10 +1,19 @@
-// src/components/TodoList.jsx
-import React from 'react';
+import React, { useState } from 'react';
 
 const TodoList = () => {
+  const [todos, setTodos] = useState([
+    { id: 1, text: 'Learn React' },
+    { id: 2, text: 'Build a Todo App' },
+  ]);
+
   return (
     <div>
       <h1>Todo List</h1>
+      <ul>
+        {todos.map(todo => (
+          <li key={todo.id}>{todo.text}</li>
+        ))}
+      </ul>
     </div>
   );
 };
