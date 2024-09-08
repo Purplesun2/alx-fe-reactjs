138371ebadfb140fc9a30a434d91a23dc893df99
Ekpen <ekpenitohan@gmail.com>
2024-09-08 21:27:29 +0000

    Configured Tailwind CSS and updated index.css

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -1,10 +1,11 @@
 module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}"
+    "./src/**/*.{js,jsx,ts,tsx}",  // Scan these files for Tailwind classes
   ],
+  darkMode: 'media',  // You can set this to 'class' for class-based dark mode
   theme: {
-    extend: {},
+    extend: {},  // You can extend Tailwind's default theme here
   },
-  plugins: [],
+  plugins: [],  // Add any plugins you want to use
 };
